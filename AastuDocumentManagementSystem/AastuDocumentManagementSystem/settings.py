d95dbd129a6a3335d90cf69ac02b885a06529428
mannyGirmaw <yohannesgirmaw23@gmail.com>
2021-08-13 20:03:36 +0300

    super user

--- AastuDocumentManagementSystem/AastuDocumentManagementSystem/settings.py
+++ AastuDocumentManagementSystem/AastuDocumentManagementSystem/settings.py
@@ -73,10 +73,19 @@ WSGI_APPLICATION = 'AastuDocumentManagementSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_document_management',
+        'USER': 'jolaravel',
+        'PASSWORD': 'st.yohannes',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
