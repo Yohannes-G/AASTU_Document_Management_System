e1b46f79321a958fe1984b815f514664f1eb2860
mannyGirmaw <yohannesgirmaw23@gmail.com>
2021-08-16 08:47:15 +0300

    models

--- AastuDocumentManagementSystem/AastuDocumentManagementSystem/settings.py
+++ AastuDocumentManagementSystem/AastuDocumentManagementSystem/settings.py
@@ -31,12 +31,24 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #Third Party Apps
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    #authentication apps
+
+    #Local Apps
+    'DocumentManagement',
+
+    #
 ]
 
 MIDDLEWARE = [
--- AastuDocumentManagementSystem/DocumentManagement/admin.py
+++ AastuDocumentManagementSystem/DocumentManagement/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import User,Role,Document,Department,Media,History,Notification,Message,Folder
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Role)
+admin.site.register(Document)
+admin.site.register(Department)
+admin.site.register(Media)
+admin.site.register(History)
+admin.site.register(Notification)
+admin.site.register(Message)
+admin.site.register(Folder)
