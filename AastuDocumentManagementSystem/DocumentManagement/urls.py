eac1c0084ad74168ebd62ec764beae962381b2b1
mannyGirmaw <yohannesgirmaw23@gmail.com>
2021-08-18 21:27:41 +0300

    rendering html

--- AastuDocumentManagementSystem/DocumentManagement/urls.py
+++ AastuDocumentManagementSystem/DocumentManagement/urls.py
@@ -3,11 +3,12 @@ from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from django.conf.urls import include
 from .views import UserViewSet
+from . import views
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
 
 urlpatterns = [
-	
+	path('',views.index, name='index'),
 	path('', include(router.urls))
 ]
--- AastuDocumentManagementSystem/DocumentManagement/views.py
+++ AastuDocumentManagementSystem/DocumentManagement/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.renderers import TemplateHTMLRenderer,JSONRenderer
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+	print('Message: Hello')
+	#template = loader.get_template('index.html')
+	return render(request,'index.html')
 
 class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+
