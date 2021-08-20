63dfd1f7962f25e1a046da63cf391786e21f438f
mannyGirmaw <yohannesgirmaw23@gmail.com>
2021-08-20 18:31:52 +0300

    user authentication

--- AastuDocumentManagementSystem/DocumentManagement/views.py
+++ AastuDocumentManagementSystem/DocumentManagement/views.py
@@ -2,34 +2,38 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+def index(request):
+    return render(request,'index.html')
 
 def signup(request):
     if request.method == "POST":
         if request.POST['password1'] == request.POST['password2']:
             try:
                 User.objects.get(username = request.POST['username'])
-                return render (request,'signup.html', {'error':'Username is already taken!'})
+                return render (request,'sign-up.html', {'error':'Username is already taken!'})
             except User.DoesNotExist:
-                user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+                user = User.objects.create_user(request.POST['username'] , password=request.POST['password1'])
                 auth.login(request,user)
-                return redirect('/')
+                return redirect('/dms-app/index')
         else:
-            return render (request,'signup.html', {'error':'Password does not match!'})
+            return render (request,'sign-up.html', {'error':'Password does not match!'})
     else:
-        return render(request,'signup.html')
+        return render(request,'sign-up.html')
 
 def login(request):
     if request.method == 'POST':
-        user = auth.authenticate(username=request.POST['username'],password = request.POST['password'])
+        print("-------------------")
+        print(request.POST['password1'])
+        user = auth.authenticate(username=request.POST['username'], password = request.POST['password1'])
         if user is not None:
             auth.login(request,user)
-            return redirect('/')
+            return redirect('/dms-app/index')
         else:
-            return render (request,'login.html', {'error':'Username or password is incorrect!'})
+            return render (request,'sign-in.html', {'error':'Username or password is incorrect!'})
     else:
-        return render(request,'login.html')
+        return render(request,'sign-in.html')
 
 def logout(request):
     if request.method == 'POST':
         auth.logout(request)
-    return redirect('/')
+    return redirect('/dms-app/login')
